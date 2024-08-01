b27575bec7abc0d1eea4a38f791402774eb508e2
braga-gustavo <gustavo.b.moraes2@gmail.com>
2024-08-01 07:07:32 -0300

    file convert

--- src/main/java/com/eventostech/api/domain/service/EventService.java
+++ src/main/java/com/eventostech/api/domain/service/EventService.java
@@ -1,20 +1,28 @@
 package com.eventostech.api.domain.service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.eventostech.api.domain.DTO.EventDTO;
 import com.eventostech.api.domain.entity.event.Event;
-import com.eventostech.api.domain.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 public class EventService {
 
     @Autowired
-    EventRepository eventRepository;
+    private AmazonS3 s3Client;
 
+    @Value("${aws.bucket.name}")
+    private String bucketName;
 
     public Event addEvent(EventDTO eventData) {
         String imgUrl = null;
@@ -34,7 +42,24 @@ public class EventService {
         return new Event();
     }
 
-    private String uploadImg(MultipartFile image) {
-        return "";
+    private String uploadImg(MultipartFile multipartFile) {
+        String imgName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+        try {
+            File file = this.converMultipartToFile(multipartFile);
+            s3Client.putObject(bucketName, imgName, file);
+            file.delete();
+            return s3Client.getUrl(bucketName, imgName).toString();
+        } catch (Exception e) {
+            System.out.println("Image upload error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private File converMultipartToFile(MultipartFile multipartFile) throws IOException {
+        File convFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+        return convFile;
     }
 }
